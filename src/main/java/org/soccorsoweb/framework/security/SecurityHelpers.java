8d2f8b589d33cff866054287434644382c3d63af
Rora <auroraisabella.camilli@student.univaq.it>
2026-07-17 16:02:29 +0200

    Add security package and security helpers

---
+++ src/main/java/org/soccorsoweb/framework/security/SecurityHelpers.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.soccorsoweb.framework.security;
+
+/**
+ *
+ * @author Aurora
+ */
+public class SecurityHelpers {
+    
+}
