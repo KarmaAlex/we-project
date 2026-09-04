245bfdcfb0251cfc62ba6b44373dee02e9e94ffa
Mattia Nanni <mattia.nanni2@student.univaq.it>
2026-09-04 15:14:11 +0300

    Update MissioneDAO.java

--- src/main/java/org/soccorsoweb/data/dao/MissioneDAO.java
+++ src/main/java/org/soccorsoweb/data/dao/MissioneDAO.java
@@ -6,6 +6,8 @@ import org.soccorsoweb.model.Missione;
 import org.soccorsoweb.model.Squadra;
 import org.soccorsoweb.data.GenericDao;
 import java.util.List;
+import org.soccorsoweb.model.Materiale;
+import org.soccorsoweb.model.Mezzo;
 import org.soccorsoweb.model.Utente;
 import org.soccorsoweb.model.enums.EsitoMissione;
 
@@ -22,5 +24,11 @@ public interface MissioneDAO extends GenericDao{
     List<Missione> getMissioniByUtente(Utente utente) throws DataException; //lista delle missioni associate ad un operatore
     
     List<Missione> getMissioniFiltrate(LocalDateTime inizio, LocalDateTime fine, EsitoMissione esito) throws DataException; //missioni filtrate in base alla data
-
+    
+    List<Missione> getStoricoMissioniByUtente(Utente utente) throws DataException;
+    
+    List<Missione> getStoricoMissioniByMezzo(Mezzo mezzo) throws DataException;
+    
+    List<Missione> getStoricoMissioniByMateriale(Materiale materiale) throws DataException;
+    
 }
